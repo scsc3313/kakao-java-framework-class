e588880629d661b93ab49a94d6e60adaa8bb20e8
HSH <scsc3313@naver.com>
2016-05-10 10:33:01 +0900

    HelloFilter

---
+++ helloworld/src/main/java/kr/ac/jejunu/sevlet/HelloFilter.java
@@ -0,0 +1,32 @@
+package kr.ac.jejunu.sevlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by HSH on 2016. 5. 10..
+ */
+public class HelloFilter implements Filter {
+
+    private final static Logger logger = LoggerFactory.getLogger(HelloFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.info("************** init *************");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        logger.info("************** filter start *************");
+        chain.doFilter(request, response);
+        logger.info("************** filter end *************");
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("************** destroy *************");
+    }
+}
